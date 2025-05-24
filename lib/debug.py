7ddb4f9d901119aac07589a4167af36f405970ec
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-05-24 12:42:26 +0300

    alchemy

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+chmod +x app/sqlalchemy_sandbox.py
 
-from sqlalchemy_sandbox import Student
 
 engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+
